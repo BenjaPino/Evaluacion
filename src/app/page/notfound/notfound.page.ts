564fb5fba60961b09bd48c5d9e80e11b5e6a9d2a
Benja <b.pinom01@gmail.com>
2021-11-12 14:06:00 -0300

    a

--- src/app/page/notfound/notfound.page.ts
+++ src/app/page/notfound/notfound.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundPage implements OnInit {
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute,private router: Router,public toastController: ToastController,
+    public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
-
+Volver(){
+  this.router.navigate(['/login'])
+}
 }
