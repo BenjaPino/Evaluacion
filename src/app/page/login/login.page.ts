248be9b9967dc193454a179ee876eb3ecd4c055c
Benja <b.pinom01@gmail.com>
2021-12-05 22:50:31 -0300

    final

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { LoadingController, ToastController } from '@ionic/angular';
+import { IViajes } from 'src/app/interfaces/iviajes';
+import { Usuarios } from 'src/app/interfaces/Usuarios';
+import { AuthService } from 'src/app/services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +14,30 @@ import { LoadingController, ToastController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   dato:string;
+  iusuarios:Usuarios[]=[];
+  login: boolean = false;
+  automovil:'SI' | 'NO' = null;
+  
+
+ 
   constructor( private activeRoute: ActivatedRoute,private router: Router,public toastController: ToastController,
-    public loadingController: LoadingController) {
+    public loadingController: LoadingController, private firestore : FirestoreService,
+    private auth : AuthService) {
     this.activeRoute.queryParams.subscribe(params=>{
       if(this.router.getCurrentNavigation().extras.state){
         this.dato=this.router.getCurrentNavigation().extras.state.dato;
         console.log(this.dato)
+      }
+    })
+    this.auth.stateUser().subscribe(res=>{
+      if (res){
+        console.log('esta logeado');
+        this.login= true;
+        this.getDatosUser(res.uid)
+      }
+      else{
+        console.log('no esta logeado');
+        this.login= false;
       }
     })
    }
@@ -26,12 +49,32 @@ export class LoginPage implements OnInit {
     }
     this.router.navigate(['/agregar'],navigationextras)
   }
-  salir(){
-    this.CerrarSesion();
-    let navigationextras: NavigationExtras={
-    }
-    this.router.navigate(['/home'],navigationextras)
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['/home'])
   }
+
+  getDatosUser(uid:string){
+    const path = 'usuarios';
+    const id = uid;
+    this.firestore.getDoc<Usuarios>(path,id).subscribe(res=>{
+      console.log('datos ->',res);
+      if (res){
+        this.automovil = res.auto,
+        this.dato = res.Nombre      
+      }
+    })
+
+  }
+  getUsuarios(uid:string){
+    const path = 'usuarios';
+    const id = uid;
+    this.firestore.getCollection<Usuarios>(path).subscribe(res => {
+      console.log('esta es la lectura',res);
+      this.iusuarios=res;
+    });
+  }
+
   async CerrarSesion() {
     const loading = await this.loadingController.create({
       cssClass: 'my-custom-class',
@@ -43,5 +86,9 @@ export class LoginPage implements OnInit {
     const { role, data } = await loading.onDidDismiss();
     console.log('Loading dismissed!');
   }
+ 
+    
+
+ 
   
 }
